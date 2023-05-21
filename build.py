ef5dc945e7cf448fed48f2d94cedd9a9a2e05f8b
LoveSy <shana@zju.edu.cn>
2023-05-21 18:13:55 +0800

    Refactor zygisk to use native bridge to inject

--- build.py
+++ build.py
@@ -230,7 +230,7 @@ def run_ndk_build(flags):
         error("Build binary failed!")
     os.chdir("..")
     for arch in archs:
-        for tgt in support_targets + ["libinit-ld.so", "libzygisk-ld.so"]:
+        for tgt in support_targets + ["libinit-ld.so"]:
             source = op.join("native", "libs", arch, tgt)
             target = op.join("native", "out", arch, tgt)
             mv(source, target)
@@ -312,9 +312,6 @@ def dump_bin_header(args):
         preload = op.join("native", "out", arch, "libinit-ld.so")
         with open(preload, "rb") as src:
             text = binary_dump(src, "init_ld_xz")
-        preload = op.join("native", "out", arch, "libzygisk-ld.so")
-        with open(preload, "rb") as src:
-            text += binary_dump(src, "zygisk_ld", compressor=lambda x: x)
         write_if_diff(op.join(native_gen_path, f"{arch}_binaries.h"), text)
 
 
@@ -362,8 +359,8 @@ def build_binary(args):
 
     flag = ""
 
-    if "magisk" in args.target or "magiskinit" in args.target:
-        flag += " B_PRELOAD=1"
+    if "magisk" in args.target:
+        flag += " B_MAGISK=1"
 
     if "magiskpolicy" in args.target:
         flag += " B_POLICY=1"
@@ -383,12 +380,9 @@ def build_binary(args):
     if flag:
         run_ndk_build(flag)
 
-    # magiskinit and magisk embeds preload.so
+    # magiskinit embeds preload.so
 
     flag = ""
-
-    if "magisk" in args.target:
-        flag += " B_MAGISK=1"
 
     if "magiskinit" in args.target:
         flag += " B_INIT=1"
